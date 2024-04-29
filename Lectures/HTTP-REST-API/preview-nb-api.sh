ec72b87e5a8da0de9250ca63f376b5e5cbac3a3a
jegesm <david.visontai@ttk.elte.hu>
2024-04-29 20:34:27 +0200

    rest lecture

---
+++ Lectures/HTTP-REST-API/preview-nb-api.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "API URL is at https://${SERVERNAME}/${REPORT_URL}"
+
+
+jupyter-kernelgateway --KernelGatewayApp.port=$REPORT_PORT --KernelGatewayApp.api=notebook-http --KernelGatewayApp.ip=0.0.0.0 --KernelGatewayApp.allow_origin=* --KernelGatewayApp.seed_uri=$1 --KernelGatewayApp.base_url="/${REPORT_URL}/"
