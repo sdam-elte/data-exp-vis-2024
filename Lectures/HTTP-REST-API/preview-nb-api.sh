c3345494717e8b072ed197354cbfe0d42ded9cf2
jegesm <david.visontai@ttk.elte.hu>
2024-04-29 22:00:41 +0200

    added NLP assignment

--- Lectures/HTTP-REST-API/preview-nb-api.sh
+++ Lectures/HTTP-REST-API/preview-nb-api.sh
@@ -3,4 +3,4 @@
 echo "API URL is at https://${SERVERNAME}/${REPORT_URL}"
 
 
-jupyter-kernelgateway --KernelGatewayApp.port=$REPORT_PORT --KernelGatewayApp.api=notebook-http --KernelGatewayApp.ip=0.0.0.0 --KernelGatewayApp.allow_origin=* --KernelGatewayApp.seed_uri=$1 --KernelGatewayApp.base_url="/${REPORT_URL}/"
+poetry run jupyter-kernelgateway --KernelGatewayApp.port=$REPORT_PORT --KernelGatewayApp.api=notebook-http --KernelGatewayApp.ip=0.0.0.0 --KernelGatewayApp.allow_origin=* --KernelGatewayApp.seed_uri=$1 --KernelGatewayApp.base_url="/${REPORT_URL}/"
